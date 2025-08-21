bf8208526045e037e66e8304186b2d89db63778c
Danning Zhan <danning.zhan@hotmail.com>
2025-08-21 11:34:41 +0200

    init commit

---
+++ src/editor.py
@@ -0,0 +1,17 @@
+from methods import *
+from generators import *
+
+
+
+
+class editor:
+    def __init__(self, datasets):
+        self.originalDataset = datasets
+
+    def addError(self, errorType, ratio):
+        """
+        Add error
+        """
+        pass
+
+    
